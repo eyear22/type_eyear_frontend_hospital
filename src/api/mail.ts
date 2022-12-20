eb612e0e35f5ea2c64e7af2c2ad0708f86ed5adb
naerim <naerim1119@gmail.com>
2022-12-20 12:28:42 +0900

    [CHORE] API 헤더추가

--- src/api/mail.ts
+++ src/api/mail.ts
@@ -1,6 +1,9 @@
 import api from "./client";
+import { getCookie } from "../util/cookie";
 
 const BASE_URL = "http://localhost:3334";
 
 export const getMailDetailAPI = (id: number) =>
-  api.get(`${BASE_URL}/post/detail/${id}`);
+  api.get(`${BASE_URL}/post/detail/${id}`, {
+    headers: { Authorization: `Bearer ${getCookie("access_token")}` },
+  });
